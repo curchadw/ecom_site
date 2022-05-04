347a8c9df0c37bfa42204ef3ed72947fa7249b6d
laure <curtisdeveloper@gmail.com>
2022-05-03 19:42:12 -0500

    provided context for products

---
+++ ecom_site/src/shop-data/shop-data.js
@@ -0,0 +1,16 @@
+[
+    {
+        id: 1,
+        title: 'Studio Bag',
+        description: 'The Studio Bag is identical to its predecessor plus a few more inches to love. Transition it from satchel to backpack to crossbody simply by changing straps. Carry it wherever you go - it will be ready to adapt to your needs and keep up with your lifestyle.',
+        imageUrl: 'https://i.ibb.co/PcXVJ8m/studiobag.jpg',
+        price: 15,
+      },
+      {
+        id: 2,
+        title: 'Cumulus',
+        description: 'Light as air. The Cumulus is encased in cloud-like waterproof nylon and has a spacious main interior. Collapse it for easy travel and snap it shut with magnetic closures.',
+        imageUrl: 'https://i.ibb.co/NtpJ0XQ/cumulus-olive.png',
+        price: 28,
+      },
+]    
